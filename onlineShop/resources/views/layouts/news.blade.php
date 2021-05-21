41e02bdd3f5d8f6b2300dd79f2b68c3c6e39d15a
Zaynutdinov-Zufarbek <zufarbekz@bk.ru>
2021-05-21 20:37:28 +0300

    Content for news section added

--- onlineShop/resources/views/layouts/news.blade.php
+++ onlineShop/resources/views/layouts/news.blade.php
@@ -3,7 +3,7 @@
         <div class="menu-content pb-60 col-lg-10">
             <div class="title text-center">
                 <h1 class="mb-10">What's the news?!</h1>
-                <p>Who are in extremely love with eco friendly system.</p>
+                <p>Latest snickers workwear range and news.</p>
             </div>
         </div>
     </div>
@@ -12,19 +12,19 @@
             <div class="single-course">
                 <span class="lnr lnr-rocket"></span>
                 <a href="#">
-                    <h4>High Performance</h4>
+                    <h4>Solid Gear Footwear</h4>
                 </a>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisic ing elit, seddo eiusmod tempor incid.idunt ut labore et dolore magna aliqua.
+                    Solid Gear Footwear is recommended by Snickers Workwear and part of the Hultafors Family. Designed to be stylish yet comfortable and light by using the latest metal and metal-free components.
                 </p>
             </div>
             <div class="single-course">
                 <span class="lnr lnr-cog"></span>
                 <a href="#">
-                    <h4>High Performance</h4>
+                    <h4>Adidas Originals Forum</h4>
                 </a>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisic ing elit, seddo eiusmod tempor incid.idunt ut labore et dolore magna aliqua.
+                    Add a fresh-looking Forum 84 Low to your rotation; this one arrives with white and red colourway with all the recognisable features that define this classic. A staple ankle strap sits up top for a secure feel, plus there's a lace closure looping down beneath.
                 </p>
             </div>
         </div>
@@ -35,19 +35,19 @@
             <div class="single-course">
                 <span class="lnr lnr-apartment"></span>
                 <a href="#">
-                    <h4>High Performance</h4>
+                    <h4>Puma Suede Vintage</h4>
                 </a>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisic ing elit, seddo eiusmod tempor incid.idunt ut labore et dolore magna aliqua.
+                    An all-time classic, the PUMA Suede is a terrace legend that first surfaced back in 1968. The shoe was originally a unique alternative to the usual leather b-ball shoes around at the time. Premium suede features up top on this shoe.
                 </p>
             </div>
             <div class="single-course">
                 <span class="lnr lnr-phone"></span>
                 <a href="#">
-                    <h4>High Performance</h4>
+                    <h4>Nike SB BLZR Court DVDL</h4>
                 </a>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisic ing elit, seddo eiusmod tempor incid.idunt ut labore et dolore magna aliqua.
+                    The Nike SB BLZR Court DVDL has been designed with the pro skater Daan Van Der Linden, offering a unique look for the silhouette. Skate-inspired, this sturdy shoe features tough canvas on the upper with a smooth leather mudguard on the lateral side.
                 </p>
             </div>
         </div>
