1df28f79685cb3e027af19dda271e5fd573854f5
Zaynutdinov-Zufarbek <zufarbekz@bk.ru>
2021-05-21 20:56:00 +0300

    Content for services section added

--- onlineShop/resources/views/layouts/service.blade.php
+++ onlineShop/resources/views/layouts/service.blade.php
@@ -10,17 +10,19 @@
     <div class="row">
         <div class="col-lg-4 col-md-6">
             <div class="single-service">
-                <h4><span class="lnr lnr-user"></span>Expert Technicians</h4>
+                <h4><span class="lnr lnr-user"></span>Emergency Services and Military Discount</h4>
                 <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    Calling emergency services – you can get 20% off.
+                    All emergency service & defence workers can benefit from 20% discount.
                 </p>
             </div>
         </div>
         <div class="col-lg-4 col-md-6">
             <div class="single-service">
-                <h4><span class="lnr lnr-license"></span>Professional Service</h4>
+                <h4><span class="lnr lnr-license"></span>Free Uzb Standard Delivery </h4>
                 <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    For orders over $70 we offer a free delivery across the country.
+                    Usually delivered within 3-5 working days.
                 </p>
             </div>
         </div>
@@ -28,23 +30,34 @@
             <div class="single-service">
                 <h4><span class="lnr lnr-phone"></span>Great Support</h4>
                 <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    If you require an exchange.
+                    Your garment will need to be returned to us within 28 days of delivery the item will need to have all tags in place. </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+            <div class="single-service">
+                <h4><span class="lnr lnr-rocket"></span>Only Top Brands</h4>
+                <p>
+                    The North Face |
+                    Nike |
+                    Vans |
+                    Stussy |
+                    Pleasures |
+                    Adidas Originals |
+                    Carhartt WIP |
+                    Converse |
+                    Jordan |
+                    New Balance |
+                    Reebok.
                 </p>
             </div>
         </div>
         <div class="col-lg-4 col-md-6">
             <div class="single-service">
-                <h4><span class="lnr lnr-rocket"></span>Technical Skills</h4>
+                <h4><span class="lnr lnr-diamond"></span>Daily Support</h4>
                 <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                </p>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="single-service">
-                <h4><span class="lnr lnr-diamond"></span>Highly Recomended</h4>
-                <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    Our operators are ready to help you with finding the sneaker of your dream.
+                    From 9:00 to 20:00 (without the weekends).
                 </p>
             </div>
         </div>
@@ -52,7 +65,7 @@
             <div class="single-service">
                 <h4><span class="lnr lnr-bubble"></span>Positive Reviews</h4>
                 <p>
-                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                   If you still doubt about buying in our store, check out the comments from our recent clients.
                 </p>
             </div>
         </div>
