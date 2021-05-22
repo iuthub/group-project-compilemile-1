ff0cd26eba8c90677ac1a9f8a38cbc92fa8f0c10
Zaynutdinov-Zufarbek <zufarbekz@bk.ru>
2021-05-22 20:49:30 +0300

    Carousel style changed

--- onlineShop/resources/views/layouts/home.blade.php
+++ onlineShop/resources/views/layouts/home.blade.php
@@ -6,13 +6,13 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="img/s1.jpg" class="d-block w-100" alt="...">
+            <img src="img/slider1.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="img/s2.jpg" class="d-block w-100" alt="...">
+            <img src="img/slider2.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="img/s3.jpg" class="d-block w-100" alt="...">
+            <img src="img/slider3.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
